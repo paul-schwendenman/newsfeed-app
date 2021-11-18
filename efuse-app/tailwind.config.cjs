b43fcdd4bc0d853ffd2dea6c11e83cc7695943e9
Paul Schwendenman <schwendenman.paul@gmail.com>
2021-11-17 22:29:25 -0500

    Add tailwind configuration
    https://magrippis.com/blog/2021/how-to-use-tailwind-with-sveltekit

---
+++ efuse-app/tailwind.config.cjs
@@ -0,0 +1,10 @@
+// const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: ['./src/**/*.svelte', './src/**/*.css'],
+  darkMode: false,
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
