2b160aabb7730fd5dbda1a96962e3b3d8942a73f
Paul Schwendenman <schwendenman.paul@gmail.com>
2021-11-24 11:41:20 -0500

    Add styling for the post component

--- efuse-app/src/lib/Post.svelte
+++ efuse-app/src/lib/Post.svelte
@@ -1,22 +1,36 @@
 <script lang="ts">
 	import Card from "./Card.svelte";
+	import dayjs from 'dayjs';
+	import relativeTime from 'dayjs/plugin/relativeTime';
+
+	dayjs.extend(relativeTime);
 
 	export let post;
 
 </script>
 
 <Card>
-	<div>
-		<!-- <span class="text-sm">{post.authorImg}</span> -->
-		<span class="text-sm">{post.author}</span>
-		<span class="text-xs">{post.time}</span>
+	<div class="header flex my-4">
+		<div class="header-image w-16 rounded-full mx-4">
+			<img alt="{post.author}" src={post.authorImageUrl} class="rounded-full" />
+		</div>
+		<div class="flex flex-col header-details">
+			<span class="text">{post.author}</span>
+			<span class="text-sm text-blue-700">{post.location}</span>
+			<span class="text-sm">{dayjs(post.createdAt).fromNow()}</span>
+		</div>
 	</div>
-	{post.body}
+	<div class="content mx-4">
+		<div class="post text-lg my-6">
+			{post.body}
+		</div>
 
-	<div class="summary">
-		<span>{post.likes} Likes</span>
-		<span>{post.comments?.length} Comments</span>
+		<div class="summary">
+			<span>{post.likes} Likes</span>
+			<span>{post.comments?.length} Comments</span>
+		</div>
 	</div>
+
 	<svelte:fragment slot="footer">
 		<button>Like</button>
 		<button>Comment</button>
