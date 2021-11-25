bf5458d82fb2bfe3fbfe7463a7aaf2e9abef5430
Paul Schwendenman <schwendenman.paul@gmail.com>
2021-11-24 23:25:40 -0500

    Add initial comment components

---
+++ efuse-app/src/lib/Comment.svelte
@@ -0,0 +1,18 @@
+<script>
+	import Image from "./Image.svelte";
+
+	export let createdAt;
+	export let text;
+	export let author;
+	export let authorImageUrl;
+	export let likes;
+</script>
+
+<div class="flex">
+	<div>
+		<Image src={authorImageUrl} alt={author} />
+	</div>
+	<div>
+		{text}
+	</div>
+</div>
---
+++ efuse-app/src/lib/CommentList.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Comment from "./Comment.svelte";
+
+	export let comments = [];
+</script>
+
+{#each comments as comment }
+	<Comment {...comment} />
+{/each}
