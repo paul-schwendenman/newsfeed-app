9a9e9c94b857759907e56318e59a5f0d8d491bc9
Paul Schwendenman <schwendenman.paul@gmail.com>
2021-11-24 23:55:38 -0500

    Add styling to comment section

--- efuse-app/src/lib/Comment.svelte
+++ efuse-app/src/lib/Comment.svelte
@@ -1,5 +1,9 @@
 <script>
+	import dayjs from 'dayjs';
+	import relativeTime from 'dayjs/plugin/relativeTime';
 	import Image from "./Image.svelte";
+
+	dayjs.extend(relativeTime);
 
 	export let createdAt;
 	export let text;
@@ -8,11 +12,23 @@
 	export let likes;
 </script>
 
-<div class="flex">
-	<div>
+<div class="flex items-center my-4">
+	<div class="mr-4">
 		<Image src={authorImageUrl} alt={author} />
 	</div>
-	<div>
-		{text}
+	<div class="w-full p-4 bg-gray-300 rounded-lg text-gray-900">
+		<div class="flex justify-between w-full mr-8">
+			<span class="leading-8">{author}</span>
+			<span class="leading-8 text-gray-500">{dayjs(createdAt).fromNow()}</span>
+		</div>
+		<div>
+			{text}
+		</div>
+		<div class="text-gray-500">
+			<span class="mr-2">{likes} likes</span> |
+			<button class="m-2"><i class="fas fa-heart"></i> Like</button> |
+			<button class="m-2"><i class="fas fa-pencil-alt"></i> Edit</button> |
+			<button class="m-2"><i class="fas fa-trash"></i> Delete</button>
+		</div>
 	</div>
 </div>
