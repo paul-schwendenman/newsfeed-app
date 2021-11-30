52ec885eae6b4fbd124ad26e9492b4d89fdbc7b9
Paul Schwendenman <schwendenman.paul@gmail.com>
2021-11-29 19:09:23 -0500

    Refactor mocks to use jest functions

--- __mocks__/dayjs.js
+++ __mocks__/dayjs.js
@@ -1,7 +1,9 @@
-const dayjs = () => ({
-	fromNow: () => {}
-});
+const dayjs = jest.fn().mockImplementation(() => ({
+	fromNow: () => {
+		return "2 days ago"
+	},
+}));
 
-dayjs.extend = () => {};
+dayjs.extend = jest.fn()
 
 export default dayjs;
