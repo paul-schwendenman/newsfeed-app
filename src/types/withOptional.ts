8027bf30ed649bce04382a1930a093bdac498df0
Paul Schwendenman <schwendenman.paul@gmail.com>
2023-03-16 00:53:35 -0400

    Add source for "WithOptional"

--- src/types/withOptional.ts
+++ src/types/withOptional.ts
@@ -1,3 +1,6 @@
+// Source: https://github.com/Microsoft/TypeScript/issues/25760#issuecomment-405931434
+
 type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+
 export type WithOptional<T, K extends keyof T> = Omit<T, K> &
   Partial<Pick<T, K>>;
