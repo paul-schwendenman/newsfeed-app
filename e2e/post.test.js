72f8829affc72b4d88fee58ba6c225ba99954040
Paul Schwendenman <schwendenman.paul@gmail.com>
2023-03-16 11:19:33 -0400

    Refactor test structure

--- e2e/post.test.js
+++ e2e/post.test.js
@@ -1,29 +1,33 @@
 import { test, expect } from "@playwright/test";
 
-test.describe("add a post", () => {
+test.describe("post", () => {
   test.beforeEach(async ({ page, baseURL }) => {
     await page.goto(baseURL);
 
     await page.waitForSelector("#app");
   });
 
-  test("pressing button works", async ({ page }) => {
-    await page.getByPlaceholder("What's on your mind?").click();
-    await page
-      .getByPlaceholder("What's on your mind?")
-      .fill("This is a post with a button");
-    await page.getByRole("button", { name: "Post" }).click();
+  test.describe("add a post", () => {
+    test("pressing button works", async ({ page }) => {
+      await page.getByPlaceholder("What's on your mind?").click();
+      await page
+        .getByPlaceholder("What's on your mind?")
+        .fill("This is a post with a button");
+      await page.getByRole("button", { name: "Post" }).click();
 
-    const content = page.locator(".post .post-body").locator("nth=0");
-    expect(content).toHaveText("This is a post with a button");
-  });
+      const content = page.locator(".post .post-body").locator("nth=0");
+      expect(content).toHaveText("This is a post with a button");
+    });
 
-  test("pressing enter works", async ({ page }) => {
-    await page.getByPlaceholder("What's on your mind?").click();
-    await page.getByPlaceholder("What's on your mind?").fill("This is a post");
-    await page.getByPlaceholder("What's on your mind?").press("Enter");
+    test("pressing enter works", async ({ page }) => {
+      await page.getByPlaceholder("What's on your mind?").click();
+      await page
+        .getByPlaceholder("What's on your mind?")
+        .fill("This is a post");
+      await page.getByPlaceholder("What's on your mind?").press("Enter");
 
-    const content = page.locator(".post .post-body").locator("nth=0");
-    expect(content).toHaveText("This is a post");
+      const content = page.locator(".post .post-body").locator("nth=0");
+      expect(content).toHaveText("This is a post");
+    });
   });
 });
